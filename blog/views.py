13b5741c2847641905d4e431fcd0c24fce465412
Alfonso Balao <alfonso.balao2@gmail.com>
2022-10-26 04:12:59 +0800

    added views & templates for blog-home and blog-about, added CSS as well

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AlfonsoB',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 25, 2022'
+    },
+    {
+        'author': 'alfsbalao',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 26, 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context ={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
